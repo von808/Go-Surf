5c0be1e7e594803c1628ce8e052667b2b47492ae
Alexey Sergeev <sergeevalexey808@gmail.com>
2023-03-02 15:12:15 +0700

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    $('.header__slider').slick({
+      infinite: true,
+      fade: true,
+      prevArrow: '<img class="slider__arrows slider__arrows-left" src="images/arrow-left.svg" alt="">',
+      nextArrow: '<img class="slider__arrows slider__arrows-right" src="images/arrow-right.svg" alt="">',
+      asNavFor: '.slider-dots',
+    });
+
+    $('.slider-dots').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      asNavFor: '.header__slider-for',
+    });
+
+})
---
+++ app/readme.md
@@ -0,0 +1,3 @@
+# Git and GitHub
+
+first perository
