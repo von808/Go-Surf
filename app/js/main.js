c966936b4678e2f25a0491e8308612a722cd5e3b
Alexey Sergeev <sergeevalexey808@gmail.com>
2023-03-08 17:41:19 +0700

    update block surf

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,12 @@ $(function () {
       asNavFor: '.header__slider-for',
     });
 
+    $('.surf__slider').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<img class="slider__arrows slider__arrows-left" src="images/arrow-left.svg" alt="">',
+      nextArrow: '<img class="slider__arrows slider__arrows-right" src="images/arrow-right.svg" alt="">',
+      asNavFor: '',
+    });
+
 })
