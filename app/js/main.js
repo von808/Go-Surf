40ec879709a3c9bc4ff833bfd435a4afb6bbbcd0
Alexey Sergeev <sergeevalexey808@gmail.com>
2023-03-24 19:04:08 +0700

    block surf done

--- app/js/main.js
+++ app/js/main.js
@@ -7,19 +7,26 @@ $(function () {
       nextArrow: '<img class="slider__arrows slider__arrows-right" src="images/arrow-right.svg" alt="">',
       asNavFor: '.slider-dots',
     });
-
     $('.slider-dots').slick({
       slidesToShow: 4,
       slidesToScroll: 4,
       asNavFor: '.header__slider-for',
     });
 
-    $('.surf__slider').slick({
+    $('.surf-slider').slick({
       slidesToShow: 4,
       slidesToScroll: 1,
+      focusOnSelect: true,
       prevArrow: '<img class="slider__arrows slider__arrows-left" src="images/arrow-left.svg" alt="">',
       nextArrow: '<img class="slider__arrows slider__arrows-right" src="images/arrow-right.svg" alt="">',
-      asNavFor: '',
+      asNavFor: '.surf-slider__map',
+    });
+    $('.surf-slider__map').slick({
+      slidesToShow: 8,
+      slidesToScroll: 1,
+      arrows: false,
+      focusOnSelect: true,
+      asNavFor: '.surf-slider',
     });
 
 })
